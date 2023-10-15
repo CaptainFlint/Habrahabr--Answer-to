000ccd98ae14969c0a104dfa9a68d073e8ecbfe3
Konstantin Vlasov <support@flint-inc.ru>
2023-10-16 02:27:06 +0300

    Adapted to the new Habr interface

--- answer_to.user.js
+++ answer_to.user.js
@@ -1,42 +1,33 @@
-﻿// Answer to...
+﻿// Habr: Answer to...
+// Forked by CaptainFlint and adapted to the new Habr interface
+// Original repository:
 // https://github.com/Inversion-des/Habrahabr--Answer-to
 
 // ==UserScript==
 // @name            Habr: Answer to...
 // @description     Shows the comment for which this comment is an answer
 // @namespace       Habrahabr
-// @version         1.3.10
+// @version         1.4.0
 // @include         https://habr.com/*
-// @updateURL       https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.meta.js
-// @downloadURL     https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
-// @supportURL      https://github.com/Inversion-des/Habrahabr--Answer-to/issues
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @grant           none
 // ==/UserScript==
 
 
-
-/*
-класс стрелочки вверх — up-to-parent
-href — #comment_1706843
-
-ID ответа — comment_1706843
-	класс заголовка — div info
-	класс тела — div message
-*/
-
-"use strict";
+'use strict';
 
 !function(win) {
+
+this.$ = this.jQuery = jQuery.noConflict(true);
 
 if (window != window.top) {
 	return;
 }
 var doc = win.document;
 
-win.addEventListener("load", function() {
-	var comments_cont = doc.getElementById('comments');
+win.addEventListener('load', function() {
+	var comments_cont = (doc.getElementsByClassName('tm-article-comments'))[0];
 	if (comments_cont) {
-
 		var msgStyle = "\
 			background-color: white;\
 			padding: 3px 2px 0 5px;\
@@ -48,50 +39,57 @@ win.addEventListener("load", function() {
 			box-shadow: 0px 3px 12px 3px rgba(0, 0, 0, 0.3);";
 
 		// готовим контейнер для просмотра
-		win.msgContainer_cont = doc.createElement("div");
-		win.msgContainer_cont.className = "content-list__item_comment comments_list_answerTo";
+		win.msgContainer_cont = doc.createElement('article');
+		win.msgContainer_cont.className = 'tm-comment-thread__comment';
 		win.msgContainer_cont.innerHTML = '<div class="comment" style="' + msgStyle + '"></div>';
-		win.msgContainer_cont.style.cssText = "position: fixed; top: 0px; left: 0px; display: none; z-index: 199; margin: 0 !important; padding: 0 !important; overflow: visible; text-align: left;";
+		win.msgContainer_cont.style.cssText = 'position: fixed; top: 0px; left: 0px; display: none; z-index: 199; margin: 0 !important; padding: 0 !important; overflow: visible; text-align: left;';
 		doc.body.appendChild(win.msgContainer_cont);
 		win.msgContainer = win.msgContainer_cont.firstChild;
 
  		// прописываем ховер стрелочкам всех комментов
-		var arrows = comments_cont.getElementsByClassName('js-comment_parent');
+		var arrows = comments_cont.getElementsByClassName('tm-comment__icon');
 		$.each(arrows, activateArrow);
 
 		// таймер для активации новых комментариев
-		setInterval(function() {
-			var $ = win.jQuery;
-			if ($) {
-				var newComments = $('.comment .is_new .to_parent');
-				for (var i = 0, li = newComments.length; i < li; i++) {
-					var link = newComments[i];
-					if (link.getAttribute('oninit') != 'activaded') {
-						activateArrow(link);
-						link.setAttribute('oninit', 'activaded');
-					}
-				}
-			}
-		}, 2000);
+//		setInterval(function() {
+//			var $ = win.jQuery;
+//			if ($) {
+//				var newComments = $('.comment .is_new .to_parent');
+//				for (var i = 0, li = newComments.length; i < li; i++) {
+//					var link = newComments[i];
+//					if (link.getAttribute('oninit') != 'activaded') {
+//						activateArrow(link);
+//						link.setAttribute('oninit', 'activaded');
+//					}
+//				}
+//			}
+//		}, 2000);
 
 		// на клик — прячем коммент
-		win.addEventListener("mousedown", hideTargetComment, false);
+		win.addEventListener('mousedown', hideTargetComment, false);
 
 	}   // if comments_cont
 
 }, false);
 
 function activateArrow(index, arrEl) {
-	arrEl.addEventListener("mouseover", function(){
-		showTargetComment(this.getAttribute('href'), arrEl);
+	// Get the parent comment
+	// First, go up to the current comment's root
+	var parents = $(arrEl).parentsUntil('section.tm-comment-thread');
+	// Now up to the parent comment's root:
+	// section.tm-comment-thread (this element's tree) -> div.tm-comment-thread__children (parent element's children) -> section.tm-comment-thread (parent element's tree)
+	var parent_comment = $(parents[parents.length - 1]).parent().parent().parent();
+	// Extract the anchor of the parent comment:
+	// -> article.tm-comment-thread__comment -> a.tm-comment-thread__target[name="comment_26058422"]
+	var anchor = (parent_comment.find('a.tm-comment-thread__target'))[0].getAttribute('name');
+	arrEl.addEventListener('mouseover', function(){
+		showTargetComment(anchor, arrEl);
 	}, false);
-	arrEl.addEventListener("mouseout", hideTargetComment, false);
+	arrEl.addEventListener('mouseout', hideTargetComment, false);
 }
 
-function showTargetComment(href, arrEl) {
-	// ищем объект по id
-	var id = href.replace(/^.*?#/, '');
-	var target = doc.getElementById(id);
+function showTargetComment(anchor, arrEl) {
+	var target = doc.getElementsByName(anchor)[0].parentNode;
 
 	// чистим контейнер
 	while (win.msgContainer.childNodes.length) {
@@ -99,30 +97,21 @@ function showTargetComment(href, arrEl) {
 	}
 
 	// заполняем контейнер новым комментом
-	var comment_body = target;
-//	for (var i = 0, l = target.childNodes.length; i < l; i++) {
-//		var tmp = target.childNodes[i];
-//		if (/comment_body/.test(tmp.className)) {
-//			comment_body = tmp;
-//			break;
-//		}
-//	}
-	if (comment_body) {
-		for (var i = 0, l = comment_body.childNodes.length; i < l; i++) {
-			var tmp = comment_body.childNodes[i];
-			if (/head|message/.test(tmp.className)) {
-				win.msgContainer.appendChild(tmp.cloneNode(true));
-			}
-			// выходим из цикла
-			if (tmp.className == "message") {
-				break;
+	if (target) {
+		for (var i = 0, l = target.childNodes.length; i < l; i++) {
+			var tmp = target.childNodes[i];
+			if (tmp.tagName == 'DIV') {
+				var tmpCopy = tmp.cloneNode(true);
+				// Hack to get rid of the horizontal scrollbar in the popup
+				$(tmpCopy).find('header').css('margin-right', '0');
+				win.msgContainer.appendChild(tmpCopy);
 			}
 		}
 	}
 
 	// подгоняем ширину под блок комментариев
-	var pageComments_cont = doc.getElementById('comments');
-	win.msgContainer_cont.style.width = )pageComments_cont.offsetWidth + absLeft(pageComments_cont) + 2) + 'px';
+	var pageComments_cont = (doc.getElementsByClassName('tm-article-comments'))[0];
+	win.msgContainer_cont.style.width = (pageComments_cont.offsetWidth + absLeft(pageComments_cont) + 2) + 'px';
 
 
 	var targetTop = absTop(target);
@@ -134,7 +123,7 @@ function showTargetComment(href, arrEl) {
 	// height auto adjustment
 	if (targetTop < windowScrollPos) {
 		var currentCommentTop = absTop(arrEl.parentNode.parentNode);
-		var targetCommentHeight = target.offsetHeight; 
+		var targetCommentHeight = target.offsetHeight;
 		var delta = (windowScrollPos + targetCommentHeight) - (currentCommentTop - 10);
 		win.msgContainer.style.maxHeight = (delta > 0 ? (targetCommentHeight - delta + 'px') : '');
 	}
@@ -143,7 +132,7 @@ function showTargetComment(href, arrEl) {
 	}
 
 	// показываем
-	win.msgContainer.parentNode.style.display = "block";
+	win.msgContainer.parentNode.style.display = 'block';
 
 	// отступы
 	win.msgContainer.style.marginLeft = (absLeft(target) - 5) + 'px';
@@ -151,7 +140,7 @@ function showTargetComment(href, arrEl) {
 }   // showTargetComment
 
 function hideTargetComment() {
-	win.msgContainer.parentNode.style.display = "none";
+	win.msgContainer.parentNode.style.display = 'none';
 }
 
 function absLeft(o) {
