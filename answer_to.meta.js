1ee8c60f6fdf6bf9408d56213f3f39985a6e3aba
Konstantin Vlasov <support@flint-inc.ru>
2023-10-16 02:53:43 +0300

    Updated Readme and meta

--- answer_to.meta.js
+++ answer_to.meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name			Answer to...
-// @description		Shows the comment for which this comment is an answer
-// @namespace		Habrahabr
-// @version        	1.3.10
-// @include			https://geektimes.com/*
-// @include			https://habr.com/*
-// @updateURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.meta.js
-// @downloadURL		https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
-// @supportURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/issues
+// @name            Habr: Answer to...
+// @description     Shows the comment for which this comment is an answer
+// @namespace       Habrahabr
+// @version         1.4.0
+// @include         https://habr.com/*
+// @require         https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @grant           none
+// @updateURL       https://github.com/CaptainFlint/Habrahabr--Answer-to/raw/master/answer_to.meta.js
+// @downloadURL     https://github.com/CaptainFlint/Habrahabr--Answer-to/raw/master/answer_to.user.js
+// @supportURL      https://github.com/CaptainFlint/Habrahabr--Answer-to/issues
 // ==/UserScript==
--- answer_to.user.js
+++ answer_to.user.js
@@ -11,6 +11,9 @@
 // @include         https://habr.com/*
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @grant           none
+// @updateURL       https://github.com/CaptainFlint/Habrahabr--Answer-to/raw/master/answer_to.meta.js
+// @downloadURL     https://github.com/CaptainFlint/Habrahabr--Answer-to/raw/master/answer_to.user.js
+// @supportURL      https://github.com/CaptainFlint/Habrahabr--Answer-to/issues
 // ==/UserScript==
 
 
